21eaf0bbb7bb65d06ea2e65d82c22ba7e8947a82
NoSharp <33969918+NoSharp@users.noreply.github.com>
2021-01-27 07:20:54 +0000

    Remove automatically generated copyright notice.
    Forgot to disable Rider's auto-copyright.

--- BasicEventHandler/Example.cs
+++ BasicEventHandler/Example.cs
@@ -1,19 +1,4 @@
 //   Written by NoSharp
-//   Copyright Notice
-/*
-    © 2020 Elitelupus All rights reserved. 
-
-    All rights in this application are owned by Elitelupus and legally protected under applicable intellectual
-    property laws.
-
-    Your possession, access, and use of this application or any part of the same does not 
-    transfer to you or any third party any rights, title, or interest in or to the application 
-    or intellectual property rights.
-
-    You may not use this application or any component therein for any commercial use or reproduce,
-    distribute or transmit the same in any form or by any means without the prior written consent 
-    of the owner.
-*/
 
 
 using System;
